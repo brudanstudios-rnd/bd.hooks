fff6639b5fffd0e17b40d5d01259425ef7749ba1
geosam <geosam@oneanimation.com>
2019-05-07 17:25:15 +0800

    updated category

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bd-hooks',
-    version="0.0.2",
+    version="0.0.3",
     description=(
         'This package allows to extend the core functionality using hooks.'
     ),
